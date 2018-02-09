1cbb2585d6e9caed8808b6bc59b05763439bc6bc
Rachel Miles <milesrachel9@gmail.com>
2018-02-09 12:23:28 -0700

    change to module

--- main.rb
+++ main.rb
@@ -1,53 +1,26 @@
 require 'colorize'
+require_relative 'git'
 
-def puts_git(cmd)
-  puts `git #{cmd} -h`
-end
+class Main
+  include Git
 
-def menu
-  puts '1: Enter git command'
-  puts '2: Exit'
-  choice = gets.to_i
-  case choice
-    when 1
-      puts 'Enter git command'
-      puts_git(gets.strip)
-      menu
-    when 2
-      exit
-    else
-      puts 'Invalid choice'
-      menu
+  def menu
+    puts 'MAIN MENU'.colorize(:cyan)
+    puts '1: Enter git command.'.colorize(:cyan)
+    puts '2: Exit.'.colorize(:cyan)
+    choice = gets.to_i
+    case choice
+      when 1
+        puts 'Enter git command.'.colorize(:green)
+        puts_git(gets.strip)
+        menu
+      when 2
+        exit
+      else
+        puts 'Invalid choice.'.colorize(:red)
+        menu
+    end
   end
 end
 
-menu
-
-
-
-
-# require 'colorize'
-#
-# def puts_git(cmd)
-#   puts 'git #{cmd} -h'
-# end
-#
-# def menu
-#   puts 'MAIN MENU'.colorize(:cyan)
-#   puts '1: Enter git command'.colorize(:cyan)
-#   puts '2: Exit'.colorize(:cyan)
-#   choice = gets.to_i
-#   case choice
-#     when 1
-#       puts 'Enter git command'.colorize(:purple)
-#       puts_git(gets.strip)
-#       menu
-#     when 2
-#       exit
-#     else
-#       puts "Invalid choice".colorize(:red)
-#       menu
-#   end
-# end
-#
-# menu
+Main.menu
